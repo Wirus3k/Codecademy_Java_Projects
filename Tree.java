2afea92dce6347639522516eda34fbf143971a9b
Pawel Wira <108659378+Wirus3k@users.noreply.github.com>
2022-07-27 14:03:13 +0200

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,14 @@
+public class Tree {
+    public static void main(String[] args){
+        /* Program would introduce myself and print tree
+         */
+        System.out.println("Hey there, I'm Pawel and I'm learning to code in Java");
+        System.out.println("I am going to plant a tree today!");
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("   |   ");
+        System.out.println("   |   ");
+        System.out.println("-------");
+    }
+}
